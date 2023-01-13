9fb06e8b04fc98c073c1b384e72e4c1a8134f6b3
ZoranUTF8 <janjiczoran23@gmail.com>
2023-01-13 13:26:53 +0100

    ad top 6 tours instead of 5

--- middleware/top-five-tours.js
+++ middleware/top-five-tours.js
@@ -1,6 +1,6 @@
 const aliasGetTopFive = (req, res, next) => {
   //* We prefil the query filters so we get the top 5
-  req.query.limit = "5";
+  req.query.limit = "6";
   req.query.sort = "-ratingAverage,price";
   req.query.fields = "name,price,ratingAverage,duration,summary,description";
   next();
