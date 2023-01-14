dcdc1c0b07bdbbaeacee08270238f22d1c228562
ZoranUTF8 <janjiczoran23@gmail.com>
2023-01-14 13:23:06 +0100

    add imageCover to top 6 tours

--- middleware/top-five-tours.js
+++ middleware/top-five-tours.js
@@ -2,7 +2,8 @@ const aliasGetTopFive = (req, res, next) => {
   //* We prefil the query filters so we get the top 5
   req.query.limit = "6";
   req.query.sort = "-ratingAverage,price";
-  req.query.fields = "name,price,ratingAverage,duration,summary,description";
+  req.query.fields =
+    "name,price,ratingAverage,duration,summary,description,imageCover";
   next();
 };
 
